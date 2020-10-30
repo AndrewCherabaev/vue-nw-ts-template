07ce6ccf867abd26d02f0d0df04ed07a87ffd6f7
Andrew Cherabaev <a.cherabaev@mediasoft.team>
2020-10-30 23:22:32 +0400

    vue-nw-ts-template

---
+++ README.md
@@ -0,0 +1,19 @@
+# vue-nw-ts-template
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+It also require to start NW in another process
+```
+npm run serve
+npm run nw
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+And then [download nw.js binary](https://nwjs.io/downloads/) for each platform to publish appplication
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h3>
+      Features:
+    </h3>
+    <ul>
+      <li>non-strict typescript</li>
+      <li><a href="https://github.com/vuejs/vue-class-component" target="_blank" rel="noopener"> class components </a></li>
+      <li><a href="https://github.com/kaorun343/vue-property-decorator" target="_blank" rel="noopener"> property decorators </a></li>
+      <li><a href="https://nwjs.io" target="_blank" rel="noopener"> nwjs as dev env </a></li>
+      <li>no babel at all</li>
+    </ul>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() msg: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
